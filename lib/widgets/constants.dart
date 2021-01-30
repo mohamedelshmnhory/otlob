5cdc5a4dbcf5695d16957fd8053286164862a0f3
mohamedelshmnhory <66264528+mohamedelshmnhory@users.noreply.github.com>
2021-01-30 13:52:48 +0200

    Update constants.dart

--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -42,26 +42,3 @@ const passTextFieldDecoration = InputDecoration(
   ),
 );
 
-const kOrderTextFieldDecoration = InputDecoration(
-  hintText: '      بحيث تكون من مكان واحد فقط   ( من السوبر ماركت مثلا )',
-  hintStyle: TextStyle(
-    fontSize: 12,
-  ),
-  counterStyle: TextStyle(fontSize: 0),
-  labelText: 'اكتب طلباتك...',
-  labelStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueGrey),
-  contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-  focusedBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-    borderSide: BorderSide(
-      color: Colors.blueGrey,
-    ),
-  ),
-  enabledBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-    borderSide: BorderSide(
-      color: Colors.cyan,
-      width: 2.0,
-    ),
-  ),
-);
